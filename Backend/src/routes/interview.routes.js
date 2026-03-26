4bac4227cad9ae7517c478d6e47f079ddf1076ae
OmPrakash23 <mishraomm5000@gmail.com>
2026-03-26 21:12:15 +0530

    Added interview routes for report generation and retrieval

--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -1,1 +1,60 @@
+import express from "express";
+import { authUser } from "../middlewares/auth.middleware.js";
+import {
+    generateInterViewReportController,
+    getInterviewReportByIdController,
+    getAllInterviewReportsController,
+    generateResumePdfController
+} from "../controllers/interview.controllers.js";
+import upload from "../middlewares/file.middleware.js";
 
+const interviewRouter = express.Router();
+
+/**
+ * @route POST /api/interview/
+ * @description generate new interview report
+ * @access private
+ */
+interviewRouter.post(
+    "/",
+    authUser,
+    upload.single("resume"),
+    generateInterViewReportController
+);
+
+/**
+ * @route GET /api/interview/report/:interviewId
+ * @description get interview report by interviewId
+ * @access private
+ */
+interviewRouter.get(
+    "/report/:interviewId",
+    authUser,
+    getInterviewReportByIdController
+);
+
+
+/**
+ * @route GET /api/interview/
+ * @description get all interview reports of logged in user
+ * @access private
+ */
+interviewRouter.get(
+    "/",
+    authUser,
+    getAllInterviewReportsController
+);
+
+
+/**
+ * @route POST /api/interview/resume/pdf/:interviewReportId
+ * @description generate resume PDF
+ * @access private
+ */
+interviewRouter.post(
+    "/resume/pdf/:interviewReportId",
+    authUser,
+    generateResumePdfController
+);
+
+export default interviewRouter;
