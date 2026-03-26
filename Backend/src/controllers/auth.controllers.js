19efac0a6b6e57cdef5e1bf80bac93da35131a9b
OmPrakash23 <mishraomm5000@gmail.com>
2026-03-26 21:23:05 +0530

    Implemented auth controller

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -1,1 +1,171 @@
+import userModel from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import tokenBlacklistModel from "../models/blacklist.model.js";
 
+/**
+ * @name registerUserController
+ * @description register a new user, expects username, email and password in the request body
+ * @access Public
+ */
+async function registerUserController(req, res) {
+
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            message: "Please provide username, email and password"
+        });
+    }
+
+    const isUserAlreadyExists = await userModel.findOne({
+        $or: [{ username }, { email }]
+    });
+
+    if (isUserAlreadyExists) {
+        return res.status(400).json({
+            message: "Account already exists with this email address or username"
+        });
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    });
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+    );
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        success: true,
+        statusCode: 200,
+        message: "User details fetched successfully",
+        token,
+    
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+    
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+            createdAtFormatted: new Date(user.createdAt).toLocaleString()
+        },
+    
+        security: {
+            isAuthenticated: true,
+            tokenIssued: true,
+            tokenExpiry: "1 day"
+        },
+    
+        stats: {
+            totalInterviewsGenerated: 0, // can be dynamic later
+            lastLogin: new Date().toISOString()
+        },
+    
+        links: {
+            self: "/api/auth/get-me",
+            generateInterview: "/api/interview",
+            logout: "/api/auth/logout"
+        },
+    
+        meta: {
+            requestTime: new Date().toISOString(),
+            apiVersion: "v1"
+        }
+    });
+}
+
+
+/**
+ * @name loginUserController
+ * @description login a user, expects email and password in the request body
+ * @access Public
+ */
+async function loginUserController(req, res) {
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        });
+    }
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+    );
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        message: "User loggedIn successfully.",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    });
+}
+
+
+/**
+ * @name logoutUserController
+ * @description clear token from user cookie and add the token in blacklist
+ * @access public
+ */
+async function logoutUserController(req, res) {
+    const token = req.cookies.token;
+
+    if (token) {
+        await tokenBlacklistModel.create({ token });
+    }
+
+    res.clearCookie("token");
+
+    res.status(200).json({
+        message: "User logged out successfully"
+    });
+}
+
+
+/**
+ * @name getMeController
+ * @description get the current logged in user details.
+ * @access private
+ */
+async function getMeController(req, res) {
+
+    const user = await userModel.findById(req.user.id);
+
+    res.status(200).json({
+        message: "User details fetched successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    });
+}
+
+export {registerUserController, logoutUserController, loginUserController, getMeController};
