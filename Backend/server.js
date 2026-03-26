974052b212a02e01444f0ec3aad038f673ea9d4e
OmPrakash23 <mishraomm5000@gmail.com>
2026-03-26 21:29:40 +0530

    Implemented server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,16 @@
+import app from "./src/app.js";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+// import invokeGeminiAiModel from "./src/services/ai.service.js";
+
+
+dotenv.config();
+connectDB();
+// invokeGeminiAiModel();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on the port ${PORT}`);
+});
 
