e794c73490d9da0dba9853863ef10c08507b6322
OmPrakash23 <mishraomm5000@gmail.com>
2026-03-26 21:25:42 +0530

    MongoDB connection setup

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,1 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("MongoDB connected succesfully!🚀");
+    })
+    .catch(() => {
+      console.error("Error while connecting to DB ❌");
+      process.exit(1);
+    });
+};
+
+export default connectDB;
 
